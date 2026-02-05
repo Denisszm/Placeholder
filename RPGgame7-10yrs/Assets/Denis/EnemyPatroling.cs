9380961954ecfd2ea220b6191ad34afa7a5d420a
Denisszm <stefanszm@yahoo.com>
2026-02-05 17:13:56 +0100

    Enemy Ai
    Går fram och bak mellan point A och point B

---
+++ RPGgame7-10yrs/Assets/Denis/EnemyPatroling.cs
@@ -0,0 +1,51 @@
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class EnemyPatroling : MonoBehaviour
+{
+    public GameObject pointA;
+    public GameObject pointB;
+    private Rigidbody2D rb;
+    private Animator anim;
+    private Transform currentPoint;
+    [SerializeField] public float speed = 1.0f;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        currentPoint = pointB.transform;
+        anim.SetBool("isWalking", true);
+    }
+
+    
+    void Update()
+    {
+        Vector2 point = currentPoint.position - transform.position;
+        if ( currentPoint == pointB.transform)
+        {
+            rb.linearVelocity = new Vector2(speed, 0);
+        }
+        else
+        {
+            rb.linearVelocity = new Vector2(-speed, 0);
+        }
+
+        if (Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == pointB.transform)
+        {
+            currentPoint = pointA.transform;
+        }
+        if (Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == pointA.transform)
+        {
+            currentPoint = pointB.transform;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.purple;
+        Gizmos.DrawWireSphere(pointA.transform.position, 0.5f);
+        Gizmos.DrawWireSphere(pointB.transform.position, 0.5f);
+        Gizmos.DrawLine(pointA.transform.position, pointB.transform.position);
+    }
+}
