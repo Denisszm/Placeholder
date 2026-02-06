05ff6a8a4594a263a409ce92dc0cc99b01461714
Romedaa <gaioromeda@gmail.com>
2026-02-06 18:41:11 +0100

    Mer Main Menu

--- RPGgame7-10yrs/Assets/Gaio/Scripts/MenuScript.cs
+++ RPGgame7-10yrs/Assets/Gaio/Scripts/MenuScript.cs
@@ -1,11 +1,13 @@
+using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class MenuScript : MonoBehaviour
 {
     public GameObject Arrow;
     public int CurrentArrowPosition;
-    public Vector2[] ArrowPositions = new Vector2[3];
+    public Vector3[] ArrowPositions = new Vector3[3];
 
     void Start()
     {
@@ -14,6 +16,45 @@ public class MenuScript : MonoBehaviour
 
     void Update()
     {
-       
+        //INPUT
+
+        //Arrow Input
+        if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
+        {
+            CurrentArrowPosition++;
+            if (CurrentArrowPosition > 2)
+            {
+                CurrentArrowPosition = 2;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
+        {
+            CurrentArrowPosition--;
+            if (CurrentArrowPosition < 0)
+            {
+                CurrentArrowPosition = 0;
+            }
+        }
+        
+        //Select Input
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.KeypadEnter))
+        {
+            if (CurrentArrowPosition == 0)
+            {
+                
+            }
+            else if (CurrentArrowPosition == 1)
+            {
+
+            }
+            else if (CurrentArrowPosition == 2)
+            {
+
+            }
+        }
+
+
+        //UPDATE
+        Arrow.transform.position = ArrowPositions[CurrentArrowPosition];
     }
 }
