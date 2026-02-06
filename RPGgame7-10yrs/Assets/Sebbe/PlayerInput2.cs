9f85095f16e6b849ff18ace06d8ae82ac8d9514f
saybaywhoahwhoahwhoah <sebastian.liliegren@elev.ga.lbs.se>
2026-02-06 16:58:38 +0100

    Player 1 & 2 movement basics
    Nu finns det 2 players. Båda kan röra sig höger och vänster samt hoppa.

--- RPGgame7-10yrs/Assets/Sebbe/PlayerInput2.cs
+++ RPGgame7-10yrs/Assets/Sebbe/PlayerInput2.cs
@@ -1,12 +1,12 @@
 using UnityEngine;
 
-public class PlayerInput : MonoBehaviour
+public class PlayerInput2 : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
     private Rigidbody2D rb;
     public int movespeed = 10;
-  
+
     public float jumpForce = 20f;
     public Transform groundCheck; // Drag an empty GameObject here (at player's feet)
     public float checkRadius = 0.2f;
@@ -21,15 +21,15 @@ public class PlayerInput : MonoBehaviour
     void Update()
     {
 
-        
+
         transform.rotation = Quaternion.identity;
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
 
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.RightArrow))
         {
             rb.linearVelocity = new Vector2(movespeed, rb.linearVelocity.y);
         }
-        else if (Input.GetKey(KeyCode.A))
+        else if (Input.GetKey(KeyCode.LeftArrow))
         {
             rb.linearVelocity = new Vector2(movespeed * -1, rb.linearVelocity.y);
         }
@@ -38,7 +38,7 @@ public class PlayerInput : MonoBehaviour
             rb.linearVelocity = new Vector2(0, rb.linearVelocity.y);
         }
 
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded && rb.linearVelocity.y < 0.01f)
+        if (Input.GetKeyDown(KeyCode.UpArrow) && isGrounded && rb.linearVelocity.y < 0.01f)
         {
             rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
 
