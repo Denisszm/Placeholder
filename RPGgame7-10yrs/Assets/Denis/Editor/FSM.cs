42f94d2c0664ef1c2065f724ab37d93acfe7b9e7
Denisszm <stefanszm@yahoo.com>
2026-02-17 16:58:23 +0100

    FSM updates
    du kan nu välja två states och linka dem, dock målas inget upp än

--- RPGgame7-10yrs/Assets/Denis/Editor/FSM.cs
+++ RPGgame7-10yrs/Assets/Denis/Editor/FSM.cs
@@ -2,6 +2,8 @@ using UnityEditor;
 using UnityEngine;
 using System.Collections.Generic;
 using UnityEngine.InputSystem;
+using System;
+using Unity.VisualScripting;
 
 [System.Serializable]
 class StateBox
@@ -14,6 +16,14 @@ public class FSM : EditorWindow
     private int dragIndex = -1;
     private Vector2 dragOffset;
     private List<StateBox> states = new List<StateBox>();
+    private Rect? lastCard;
+    private Rect? newCard;
+    private Vector2 lastCardPosition;
+    private Vector2 newCardPosition;
+#nullable enable annotations //string null
+    private string? lastCardName;
+    private string? newCardName;
+
     [MenuItem("Window/FSM")]
     public static void ShowExample()
     {
@@ -40,20 +50,45 @@ public class FSM : EditorWindow
                 GUI.Box(new Rect(0, 0, states[i].rect.width, states[i].rect.height), "");
                 float textHeight = 25;
                 states[i].name = GUI.TextField(new Rect(10, (states[i].rect.height - textHeight) / 2, states[i].rect.width - 20, textHeight), states[i].name);
-
             GUI.EndGroup();
-
+            void OnDrawGizmos()
+            {
+                Gizmos.color = Color.white;
+                Gizmos.DrawLine(lastCardPosition, newCardPosition);
+            }
             Event e = Event.current;
 
             
             Rect CardsArea = states[i].rect;
+
+            
             
             if ( e.type == EventType.MouseDown && CardsArea.Contains(e.mousePosition))
             {
                 dragIndex = i;
                 dragOffset = e.mousePosition - states[i].rect.position;
-                e.Use();
+                if (lastCard != null && lastCard != newCard)
+                {
+                    newCard = states[i].rect;
+                    newCardName = states[i].name;
+                    newCardPosition = states[i].rect.position;
+                    Debug.Log($"Last Card: {states[i].name} {lastCard} New Card: {states[i].name} {newCard}");
+                    lastCard = null;
+                    lastCardName = null;
+                }
+                else
+                {
+                    
+                    lastCard = states[i].rect;
+                    lastCardName = states[i].name;
+                    lastCardPosition = states[i].rect.position;
+                    newCard = null;
+                    newCardName = null;
+                    Debug.Log($"Last Card: {states[i].name} {lastCard}");
+                }
                 
+                e.Use();
+
             }
 
             if ( e.type == EventType.KeyDown && e.keyCode == KeyCode.Delete && CardsArea.Contains(e.mousePosition))
@@ -73,4 +108,5 @@ public class FSM : EditorWindow
             }
         }
     }
+    
 }
