2e393e861b6dcaca80eaa590898545ea25bb8822
Romedaa <gaioromeda@gmail.com>
2026-02-17 11:48:57 +0100

    testing scene

--- RPGgame7-10yrs/Assets/Gaio/Scripts/OptionsScript.cs
+++ RPGgame7-10yrs/Assets/Gaio/Scripts/OptionsScript.cs
@@ -3,18 +3,53 @@ using UnityEngine.SceneManagement;
 
 public class OptionsScript : MonoBehaviour
 {
+    public GameObject Arrow;
+    public int CurrentArrowPosition;
+    public Vector3[] ArrowPositions = new Vector3[3];
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        CurrentArrowPosition = 2;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.KeypadEnter))
+        //ARROW INPUT
+        if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
         {
-            SceneManager.LoadScene(1);
+            CurrentArrowPosition++;
+            if (CurrentArrowPosition > 2)
+            {
+                CurrentArrowPosition = 2;
+            }
         }
+        if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
+        {
+            CurrentArrowPosition--;
+            if (CurrentArrowPosition < 0)
+            {
+                CurrentArrowPosition = 0;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Return))
+        {
+            if (CurrentArrowPosition == 0)
+            {
+                
+            }
+            else if (CurrentArrowPosition == 1)
+            {
+                
+            }
+            else if (CurrentArrowPosition == 2)
+            {
+                SceneManager.LoadScene(1);
+            }
+        }
+
+        //UPDATE
+        Arrow.transform.position = ArrowPositions[CurrentArrowPosition];
     }
 }
