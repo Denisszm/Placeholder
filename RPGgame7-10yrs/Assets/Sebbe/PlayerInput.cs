2dae064303748ee93029b29dbbf90cd0b2a410e1
saybaywhoahwhoahwhoah <sebastian.liliegren@elev.ga.lbs.se>
2026-02-20 13:36:48 +0100

    Refined player movement + de collidar inte

--- RPGgame7-10yrs/Assets/Sebbe/PlayerInput.cs
+++ RPGgame7-10yrs/Assets/Sebbe/PlayerInput.cs
@@ -2,97 +2,88 @@ using UnityEngine;
 
 public class PlayerInput : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-
     private Rigidbody2D rb;
-    public int movespeed = 10;
-    public bool doublejump = true;
-    public int jumpcount;
+    public float movespeed = 10f;
     public float jumpForce = 20f;
-    public Transform groundCheck; // Drag an empty GameObject here (at player's feet)
+    public bool doublejump;
+    private float jumpCutMultiplier = 0.5f;
+
+    public Transform groundCheck;
     public float checkRadius = 0.2f;
-    public LayerMask whatIsGround; // Set this to "Ground" in the Inspector
+    public LayerMask whatIsGround;
+
+    private int jumpcount;
     private bool isGrounded;
-    public KeyCode MoveLeft;
-    public KeyCode MoveRight;
-    public KeyCode Jump;
+    private float horizontalInput;
+
+    private KeyCode MoveLeft, MoveRight, Jump, Dash;
     public int PlayerNumber;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        // Fryser rotation så spelaren inte trillar framåt
+        rb.freezeRotation = true;
+
         if (PlayerNumber == 1)
         {
-            MoveLeft = KeyCode.A;
-            MoveRight = KeyCode.D;
-            Jump = KeyCode.Space;
-        }
-        if (PlayerNumber == 2)
-        {
-            MoveLeft = KeyCode.LeftArrow;
-            MoveRight = KeyCode.RightArrow;
-            Jump = KeyCode.UpArrow;
-        }
-       
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-        
-        transform.rotation = Quaternion.identity;
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
-
-        if (Input.GetKey(MoveRight))
-        {
-            rb.linearVelocity = new Vector2(movespeed, rb.linearVelocity.y);
-        }
-        else if (Input.GetKey(MoveLeft))
-        {
-            rb.linearVelocity = new Vector2(movespeed * -1, rb.linearVelocity.y);
+            MoveLeft = KeyCode.A; MoveRight = KeyCode.D; Jump = KeyCode.Space;
         }
         else
         {
+            MoveLeft = KeyCode.LeftArrow; MoveRight = KeyCode.RightArrow; Jump = KeyCode.UpArrow;
+        }
+    }
+
+    void Update()
+    {
+        // 1. Samla input i Update
+        horizontalInput = 0;
+        if (Input.GetKey(MoveRight)) horizontalInput = 1;
+        if (Input.GetKey(MoveLeft)) horizontalInput = -1;
+        
+
+        // 2. Markkontroll
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+
+        if (isGrounded && rb.linearVelocity.y <= 0.1f)
+        {
+            jumpcount = doublejump ? 2 : 1;
+        }
+
+        // 3. Hopp (GetKeyDown måste vara i Update för att inte missas)
+        if (Input.GetKeyDown(Jump) && jumpcount > 0)
+        {
+            
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, 0);
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            jumpcount--;
+        }
+        if (Input.GetKeyUp(Jump))
+        {
+            // Om vi rör oss uppåt (velocity.y > 0)
+            if (rb.linearVelocity.y > 0)
+            {
+                // Multiplicera nuvarande Y-fart med t.ex. 0.5. 
+                // Detta gör att man "tappar suget" uppåt och börjar falla tidigare.
+                rb.linearVelocity = new Vector2(rb.linearVelocity.x, rb.linearVelocity.y * jumpCutMultiplier);
+            }
+        }
+    }
+
+    void FixedUpdate()
+    {
+        // 4. Applicera rörelse i FixedUpdate (Fysik-vänligt)
+        // Om ingen knapp trycks, sätt X till 0 direkt för att eliminera "back-momentum"
+        if (horizontalInput == 0)
+        {
             rb.linearVelocity = new Vector2(0, rb.linearVelocity.y);
         }
-        
-            if (isGrounded && rb.linearVelocity.y <= 0.01f)
-            {
-                if (doublejump)
-                {
-                  jumpcount = 2;
-
-               
-            }
-                else
-                {
-                  jumpcount = 1;
-                }
-            }
-
-        if (Input.GetKeyDown(Jump) && jumpcount > 0)
-            {
-            rb.linearVelocity = new Vector2(rb.linearVelocity.x, 0);
-
-            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-
-            jumpcount--;
-
-        }
-        
-        
-
-            
-
-    }
-
-    private void OnDrawGizmos()
-    {
-        if (groundCheck != null)
+        else
         {
-            // If isGrounded is true, draw Green. Otherwise, draw Red.
-            Gizmos.color = isGrounded ? Color.green : Color.red;
-            Gizmos.DrawWireSphere(groundCheck.position, checkRadius);
+            rb.linearVelocity = new Vector2(horizontalInput * movespeed, rb.linearVelocity.y);
         }
     }
+
+    
 }
